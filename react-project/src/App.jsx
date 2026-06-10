f21e647c68276d6886d163fbe71b6503809cc29c
Ajit Kumar <ajit62768@gmail.com>
2026-06-11 00:17:33 +0530

    first commit

---
+++ react-project/src/App.jsx
@@ -0,0 +1,18 @@
+import UserCard from "./components/UserCard"
+import ajit from "./assets/ajit.jpg"
+
+
+function App() {
+
+
+  return (
+  
+     <>
+
+    <UserCard name="Ajit Yadav" image={ajit} description="Software Engineer"/>
+
+     </>
+  )
+}
+
+export default App
---
+++ react-project/src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import './UserCard.css'
+
+const UserCard = (props) => {
+  return (
+    <div className="user-container">
+      <p id="user-name">{props.name}</p>
+      <img src={props.image}  id="user-image"/>
+      <p id="user-description">{props.description}</p>
+    </div>
+  )
+}
+
+export default UserCard
